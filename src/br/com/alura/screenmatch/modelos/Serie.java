efef0e495850e72cda8dcf381bedd597a7862781
Thaislaine Reis <thaiserica1304@gmail.com>
2026-01-06 13:55:31 -0300

    add files

---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,50 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private boolean ativa;
+    private int minutosPorEpisodio;
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporada) {
+        this.temporadas = temporada;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Temporadas: " + temporadas);
+        System.out.println("Ep. por temporada: " + episodiosPorTemporada);
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,76 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+    //atributos
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+    private String genero;
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    //setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    //metodos
+    public void exibeFichaTecnica() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Total de Avaliações: " + getTotalDeAvaliacoes());
+        System.out.println("Nota: " + mediaAvaliacoes());
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double mediaAvaliacoes(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
