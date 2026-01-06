ac29915768a561eab65e1c28b0d2bfe49dcdb3fd
Thaislaine Reis <thaiserica1304@gmail.com>
2026-01-06 13:53:48 -0300

    override toString

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.modelos;
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Diretor: " + diretor);
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) (mediaAvaliacoes() / 2);
+    }
+
+    //sobrescrevendo o metodo toString() para retornar o nome do filme na array
+    @Override
+    public String toString() {
+        return this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
+}
