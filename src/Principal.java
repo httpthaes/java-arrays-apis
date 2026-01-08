aea18370a8af0802b74a5ee47b4d3d9f10ad9663
Thaislaine Reis <thaiserica1304@gmail.com>
2026-01-08 12:50:04 -0300

    criando lista que pode armazenar várias classes utilizando Object

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,3 @@
-import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
-import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
-import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import java.util.ArrayList;
@@ -8,17 +5,11 @@ import java.util.ArrayList;
 public class Principal {
     static void main(String[] args) {
         //filmes
-        Filme filme1 = new Filme();
-        filme1.setNome("Sinners");
-        filme1.setAnoDeLancamento(2025);
+        Filme filme1 = new Filme("Sinners", 2025);
 
-        Filme filme2 = new Filme();
-        filme2.setNome("Titanic");
-        filme2.setAnoDeLancamento(1997);
+        Filme filme2 = new Filme("Titanic", 1997);
 
-        Filme filme3 = new Filme();
-        filme3.setNome("Homem-Aranha");
-        filme3.setAnoDeLancamento(2002);
+        Filme filme3 = new Filme("Homem-Aranha", 2002);
 
         //Lista de filmes com ArrayList
         ArrayList<Filme> listaDeFilmes = new ArrayList<>();
@@ -29,5 +20,16 @@ public class Principal {
         System.out.println("Tamanho da lista: " + listaDeFilmes.size());
         System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes);
+
+        //Lista das classes Filme e Serie utilizando Object
+        ArrayList<Object> programasFiccaoCientifica = new ArrayList<>();
+
+        Filme filmeFiccaoCientifica = new Filme("Alien, O Oitavo Passageiro", 1979);
+        Serie serieFiccaoCientifica = new Serie("Black Mirror", 2011);
+
+        programasFiccaoCientifica.add(filmeFiccaoCientifica);
+        programasFiccaoCientifica.add(serieFiccaoCientifica);
+
+        System.out.println(programasFiccaoCientifica);
     }
 }
