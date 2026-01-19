a4cb5af1a0923fb277570e69ce224e58117714b7
Thaislaine Reis <thaiserica1304@gmail.com>
2026-01-19 11:11:03 -0300

    implementando comparable

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo>{
     //atributos
     private String nome;
     private int anoDeLancamento;
@@ -77,5 +77,10 @@ public class Titulo {
 
     public double mediaAvaliacoes(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
     }
 }
