d8338530e9f139bad4794cda9787d37a000c4b73
Thais <thaiserica1304@gmail.com>
2026-03-07 17:16:46 -0300

    trata caracteres especiais na busca e adiciona tratamento de exceções

--- src/br/com/alura/screenmatch/principal/PrincipalAPI.java
+++ src/br/com/alura/screenmatch/principal/PrincipalAPI.java
@@ -8,13 +8,15 @@ import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class PrincipalAPI {
-    static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String apiKey = System.getenv("API_KEY");
 
         if (apiKey == null) {
@@ -24,29 +26,38 @@ public class PrincipalAPI {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um filme para buscar: ");
         var busca = scanner.nextLine();
+        busca = URLEncoder.encode(busca, StandardCharsets.UTF_8);
 
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=" + apiKey;
 
-        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
-        System.out.println(json);
+            String json = response.body();
+            System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
 
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
 
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println(meuTitulo);
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao converter string em number, verifique o formato da string.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro de argumento na busca, verifique o endereço.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
